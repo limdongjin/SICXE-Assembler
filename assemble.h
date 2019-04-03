45063a928aa5350b2b39a6e60f743b40095ea263
limdongjin <geniuslim27@gmail.com>
2019-04-03 10:20:43 +0900

    Statement 구현

--- assemble.h
+++ assemble.h
@@ -1,8 +1,19 @@
-//#ifndef __ASSEMBLE_H__
-//#define __ASSEMBLE_H__
-//
-//#include <stdbool.h>
-//
+#ifndef __ASSEMBLE_H__
+#define __ASSEMBLE_H__
+
+#include <stdbool.h>
+#include "opcode.h"
 //bool assemble_file_impl();
-//
-//#endif
+#define MAX_TOKENS_LENGTH 8
+
+typedef struct statement {
+    char* raw_input;
+    int token_cnt;
+    char tokens*[MAX_TOKENS_LENGTH];
+    bool comment;
+    Opcode* opcode;
+    char* raw_symbol;
+    bool tmp_bool;
+}Statement;
+
+#endif
